aeba8e0d439a12f6ebcfe6f14a1004747ffd464a
George Carder <georgercarder@gmail.com>
2021-03-20 19:49:38 -0700

    recovered KingKongUtils

--- contracts/KingKongUtils.sol
+++ contracts/KingKongUtils.sol
@@ -1,45 +1,91 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.7.0;
 
-import "./KingKongUtils.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
-contract KingKong is KingKongUtils {
-
-	constructor(address firstMember) {
-		address[2] memory noChildrenYet;
-		members[firstMember] = Member(
-			firstMember, 0, address(0), noChildrenYet);	
-		rowB.push(firstMember);
-		height = 1;
-		usingRowB = true;
+contract KingKongUtils {
+	uint256 public constant membershipFee = 1 ether;
+	mapping (address => Member) internal members;
+	uint256 internal height;
+	address[] internal rowA;
+	address[] internal rowB;
+	bool internal usingRowB;
+	struct Member {
+		address memberAddress;
+		uint256 balance;
+		address parent;
+		address[2] children;
 	}
-
-	function join() payable external {
-		require(msg.value == membershipFee, "insufficient payment.");
-		address parent = getParentFromRow();
-		Member[] memory lineage = fillLineage(parent);
-		// pay membership
-		payMembership(msg.value, lineage);
-		// new member
-		addMember(msg.sender, parent);
-
-		// accounting of rows
-		Member storage parentAsMember = members[parent];
-		if (parentAsMember.children[0] == address(0)) {
-			parentAsMember.children[0] = msg.sender;	
-			putParentBackInRow(parent);
-		} else {
-			parentAsMember.children[1] = msg.sender;	
+	function payMembership(
+		uint256 payment, Member[] memory lineage) internal {
+		for (uint256 i = lineage.length-1; i > 0; i--) {
+			payment /= 2; // should not need safemath
+			members[lineage[i].memberAddress].balance += payment;
+			// should not need safemath because of scarcity of ether	
 		}
-		updateActiveRow();
-		putNewMemberInRow(msg.sender);
+	}
+	function fillLineage(
+		address parent
+	) internal view returns(Member[] memory) {
+		bool filling = true;
+		Member memory member;
+		Member[] memory lineage = new Member[](height);
+		uint256 idx;
+		while (filling) {
+			member = members[parent];
+			lineage[idx] = member;
+			idx++;
+			if (member.parent == address(0)) {
+				filling = false;
+			}
+		}
+		return lineage;
+	}
+	function getParentFromRow() internal returns(address) {
+		address parent;
+		if (usingRowB) {
+			parent = rowB[rowB.length-1];	
+			rowB.pop();
+			return parent;
+		}
+		parent = rowA[rowB.length-1];	
+		rowA.pop();
+		return parent;
+	}
+	function putParentBackInRow(address parent) internal {
+		if (usingRowB) {
+			rowB.push(parent);
+			return;
+		}
+		rowA.push(parent);	
+	}
+	function updateActiveRow() internal {
+		if (usingRowB) {
+			if (rowB.length == 0) {
+				usingRowB = !usingRowB;
+				height++;
+			}
+			return;
+		}
+		if (rowA.length == 0) {
+			usingRowB = !usingRowB;
+			height++;
+		}
+	}
+	function putNewMemberInRow(address member) internal {
+		// recall that the new member is put in the inactive row
+		if (usingRowB) {
+			rowA.push(member);
+			return;
+		}
+		rowB.push(member);
+	}
+	function addMember(address member, address parent) internal {
+		// this is checked in that 
+		// member is msg.sender and parent is necessarily nontrivial
+		address[2] memory noChildrenYet;
+		members[member] = Member(
+			member, 0, parent, noChildrenYet);	
 	}
 
-	function withdraw(uint amount, address recipient) external {
-		require(members[msg.sender] != Member(0), "not a member!");
-		members[msg.sender].balance = 
-			members[msg.sender].balance.sub(amount);
-		(bool success, ) = recipient.call.value(amount)("");	
-		require(success, "withdraw failed!");
-	}
 }
